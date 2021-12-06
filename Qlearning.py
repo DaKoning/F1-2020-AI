5e1c7ac4c752547c054a3b6250f7caf62e23e49d
DaKoning <daaf01@live.nl>
2021-12-06 21:10:22 +0100

    testing github

--- Qlearning.py
+++ Qlearning.py
@@ -9,7 +9,7 @@ delete_progress = False
 
 if delete_progress or not exists("Qtable.npy"):
     #Q_table = np.array([['speed', 'angle', 'pos_x', 'pos_y', 'ray_dis_0', 'ray_dis_45', 'ray_dis_90', 'ray_dis_135', 'ray_dis_180', 'throttle', 'brakes', 'left_steering', 'right_steering', 'Q']])
-    Q_table = np.array([['speed', 'ray_dis_0', 'ray_dis_45', 'ray_dis_90', 'ray_dis_135', 'ray_dis_180', 'throttle', 'brakes', 'steering', 'Q']], dtype=np.object)
+    Q_table = np.array([['speed', 'ray_dis_0', 'ray_dis_45', 'ray_dis_90', 'ray_dis_135', 'ray_dis_180', 'throttle', 'brakes', 'steering', 'Q']], dtype=object)
     Q_table = np.append(Q_table, [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], axis=0)
     np.save("Qtable", Q_table)
 else:
